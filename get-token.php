4a53b044b5833f94aebb9e2ed3f0c0b882687646
Jasper de Laat <jasper.de.laat@live.nl>
2021-07-02 17:34:45 +0200

    Implemented user_id verification. Falls back to IP verification if no user_id is provided.

--- get-token.php
+++ get-token.php
@@ -1,11 +1,16 @@
 <?php
+require_once('functions.php');
 require_once('settings.php');
 
-$href = 'https://www.patreon.com/oauth2/authorize' . 
-    '?response_type=code' .
-    '&client_id=' . PATREON_CLIENT_ID .
-    '&redirect_uri=' . urlencode(PATREON_REDIRECT_URI) .
-    '&scope=' . PATREON_SCOPE;
+$data = array(
+    'response_type' => 'code',
+    'client_id'     => PATREON_CLIENT_ID,
+    'redirect_uri'  => PATREON_REDIRECT_URI,
+    'scope'         => PATREON_SCOPE,
+    'state'         => $_GET['user_id']
+);
+$query = http_build_query($data);
+$href = 'https://www.patreon.com/oauth2/authorize?' . $query;
 
 header("Location: $href");
 exit();
